b7aa882a613adb0f20bd4332ca8d2185d4eeff71
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-04-11 09:31:18 +0600

    main functionality completed except menu system

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 # bismillahir rahmanir rahim
+import random
+
+
+# this is my function for getting random number for student id
+def get_random_id():
+    return random.randint(10000, 99999)
+
+
 
 
 # The mian Student Database Class
@@ -9,14 +17,20 @@ class StudentDatabase:
         pass
 
 
-    # defininng a add_student method to insert an object of Student Class
-    def add_student(self, student_obj):
-        if type(student_obj) == Student:
-            # have to have test between those
-            # StudentDatabase.student_list.append(student_obj)
-            self.__student_list.append(student_obj)
-        else:
-            print("Please Use The Instance Of `Student` Class Only")
+    # defining a add_student method to insert an object of Student Class
+    @classmethod
+    def add_student(cls,student_obj):
+        cls.__student_list.append(student_obj)
+
+    # defining the class method for viewing all the student data
+    @classmethod
+    def view_all_students(cls):
+        for student in cls.__student_list:
+            student.view_student_info(True)
+            # churi kora style
+            # print(f"Student ID: {student._Student__student_id}, Name : {student._Student__name}, Department : {student._Student__department}, Enrollment Status : {student._Student__is_enrolled}")
+
+
 
 
 # defining the Student Class
@@ -26,4 +40,53 @@ class Student:
         self.__name = name
         self.__department = department
         self.__is_enrolled = is_enrolled
+        StudentDatabase.add_student(self)
 
+    # method for enrolling student
+    def enroll_student(self):
+        if self.__is_enrolled:
+            print(f"Student [ ID: {self.__student_id} , Name : {self.__name} ] is already enrolled :) ")
+        else:
+            self.__is_enrolled=True
+            print(f"Student [ ID : {self.__student_id}, Name : {self.__name}, Department : {self.__department}, Enrolled : {self.__is_enrolled} ] Enrollment Successful 🤓🤓🤓")
+            # print(f"ID : {self.__student_id}, Name : {self.__name}, Department : {self.__department}, Enrolled : {self.__is_enrolled}")
+
+    # method for drop student
+    def drop_student(self):
+        if self.__is_enrolled:
+            self.__is_enrolled=False
+            print(f"Student [ ID : {self.__student_id}, Name : {self.__name} ] - Dropped Successfully 😭😭😭")
+        else:
+            print(f"Student [ ID: {self.__student_id} , Name : {self.__name} ] is Not Enrolled Yet :) ")
+
+
+    # view student info method for viewing the student data
+    def view_student_info(self ,db_style=False):
+        enrollment_status="Not Enrolled"
+
+        if self.__is_enrolled:
+            enrollment_status="Enrolled"
+
+        # for my 2 type of style
+        if db_style:
+            print(f"ID : {self.__student_id},Name : {self.__name},Department :{self.__department},Enrolled : {enrollment_status}")
+        else:
+            print(f"The ID of the student {self.__name} is: {self.__student_id} .He/She is in the DEPT of {self.__department} and currently he/she is  {enrollment_status} .")
+
+
+kodu = Student(get_random_id(), "Kodu", "CSE")
+jodu = Student(get_random_id(), "Jodu", "CSE")
+modu = Student(get_random_id(), "Modu", "EEE")
+lodu = Student(get_random_id(), "Lodu", "EEE")
+sahbagi = Student(get_random_id(), "Sahbagi", "Charukola")
+
+kodu.enroll_student()
+
+jodu.view_student_info()
+kodu.view_student_info()
+
+kodu.drop_student()
+jodu.drop_student()
+modu.drop_student()
+
+# StudentDatabase.view_all_students()
